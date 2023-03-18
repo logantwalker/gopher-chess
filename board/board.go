1e03fa68b3fa9bb16477864414b2e08f4e77957e
Logan Walker <logantwalker95@gmail.com>
2023-03-17 20:49:06 -0400

    printing board from fen

--- board/board.go
+++ board/board.go
@@ -1,1 +1,80 @@
 package board
+
+import (
+	"strings"
+)
+
+const StartingFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+type Board struct {
+	state 			[]int8
+	turn 			int8
+	halfMoveClock 	int
+	fullMoves		int
+}
+
+func ParseFen(fen string) []int8 {
+	boardArr := make([]int8, 128)
+	fenArray := strings.Split(fen, " ")
+
+	i := 0
+	for j := 0; j < len(fenArray[0]); j++ {
+		piece := fenArray[0][j]
+
+		switch piece {
+		case 'p':
+			boardArr[hexBoard[i]] = BlackPawn
+		case 'r':
+			boardArr[hexBoard[i]] = BlackRook
+		case 'n':
+			boardArr[hexBoard[i]] = BlackKnight
+		case 'b':
+			boardArr[hexBoard[i]] = BlackBishop
+		case 'q':
+			boardArr[hexBoard[i]] = BlackQueen
+		case 'k':
+			boardArr[hexBoard[i]] = BlackKing
+
+
+		case 'P':
+			boardArr[hexBoard[i]] = WhitePawn
+		case 'R':
+			boardArr[hexBoard[i]] = WhiteRook
+		case 'N':
+			boardArr[hexBoard[i]] = WhiteKnight
+		case 'B':
+			boardArr[hexBoard[i]] = WhiteBishop
+		case 'Q':
+			boardArr[hexBoard[i]] = WhiteQueen
+		case 'K':
+			boardArr[hexBoard[i]] = WhiteKing
+
+
+		case '1':
+			// do nothing
+		case '2':
+			i++
+		case '3':
+			i+= 2
+		case '4':
+			i+= 3
+		case '5':
+			i+= 4
+		case '6':
+			i+= 5
+		case '7':
+			i+= 6
+		case '8':
+			i+= 7
+
+		case '/':
+			i--
+
+		default:
+			return boardArr
+		}
+		i++
+	}
+
+	return boardArr
+}
--- gopher.go
+++ gopher.go
@@ -7,34 +7,7 @@ import (
 )
 
 func main(){
-	boardArr := make([]int8, 128)
-
-	boardArr[0x00] = board.WhiteRook
-	boardArr[0x01] = board.WhiteKnight
-	boardArr[0x02] = board.WhiteBishop
-	boardArr[0x03] = board.WhiteQueen
-	boardArr[0x04] = board.WhiteKing
-	boardArr[0x05] = board.WhiteBishop
-	boardArr[0x06] = board.WhiteKnight
-	boardArr[0x07] = board.WhiteRook
-
-	for i := 0x10; i < 0x18; i++ {
-		boardArr[i] = board.WhitePawn
-	}
-
-	boardArr[0x70] = board.BlackRook
-	boardArr[0x71] = board.BlackKnight
-	boardArr[0x72] = board.BlackBishop
-	boardArr[0x73] = board.BlackQueen
-	boardArr[0x74] = board.BlackKing
-	boardArr[0x75] = board.BlackBishop
-	boardArr[0x76] = board.BlackKnight
-	boardArr[0x77] = board.BlackRook
-
-	for i := 0x60; i < 0x68; i++ {
-		boardArr[i] = board.BlackPawn
-	}
-
+	boardArr := board.ParseFen("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2")
 	for i := 0x70; i >= 0x00; i -= 0x10 {
 		for j := 0; j < 8; j++ {
 			square := i + j

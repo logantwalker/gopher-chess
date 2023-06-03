0a97cb60da448d66d5589a0163304049187c9285
Logan Walker <logantwalker95@gmail.com>
2023-06-03 15:51:04 -0400

    removing redundant type declarations

--- engine/generator_test.go
+++ engine/generator_test.go
@@ -5,26 +5,26 @@ import "testing"
 func TestGenerateMovesForDefaultBoardPosition(t *testing.T) {
 
 	expected := []Move{
-		Move{From: A2, To: A3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: A2, To: A4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: B2, To: B3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: B2, To: B4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: C2, To: C3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: C2, To: C4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: D2, To: D3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: D2, To: D4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: E2, To: E3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: E2, To: E4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: F2, To: F3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: F2, To: F4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: G2, To: G3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: G2, To: G4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: H2, To: H3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: H2, To: H4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: B1, To: A3, MovedPiece: WhiteKnight, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: B1, To: C3, MovedPiece: WhiteKnight, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: G1, To: F3, MovedPiece: WhiteKnight, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: G1, To: H3, MovedPiece: WhiteKnight, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: A2, To: A3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: A2, To: A4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: B2, To: B3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: B2, To: B4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: C2, To: C3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: C2, To: C4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: D2, To: D3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: D2, To: D4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: E2, To: E3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: E2, To: E4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: F2, To: F3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: F2, To: F4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: G2, To: G3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: G2, To: G4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: H2, To: H3, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: H2, To: H4, MovedPiece: WhitePawn, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: B1, To: A3, MovedPiece: WhiteKnight, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: B1, To: C3, MovedPiece: WhiteKnight, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: G1, To: F3, MovedPiece: WhiteKnight, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: G1, To: H3, MovedPiece: WhiteKnight, Special: moveOrdinary, Content: Empty, Promoted: Empty},
 	}
 
 	doTestMovesForFEN(defaultFEN, expected, t)
@@ -35,10 +35,10 @@ func TestGenerateMovesForCheckPositionToCreateAllEscapeMovesForKing(t *testing.T
 	fen := "8/7p/1R2k1p1/3pp1P1/7P/7r/8/5K2 b - - 3 39"
 
 	expected := []Move{
-		Move{From: E6, To: E7, MovedPiece: BlackKing, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: E6, To: D7, MovedPiece: BlackKing, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: E6, To: F7, MovedPiece: BlackKing, Special: moveOrdinary, Content: Empty, Promoted: Empty},
-		Move{From: E6, To: F5, MovedPiece: BlackKing, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: E6, To: E7, MovedPiece: BlackKing, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: E6, To: D7, MovedPiece: BlackKing, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: E6, To: F7, MovedPiece: BlackKing, Special: moveOrdinary, Content: Empty, Promoted: Empty},
+		{From: E6, To: F5, MovedPiece: BlackKing, Special: moveOrdinary, Content: Empty, Promoted: Empty},
 	}
 
 	doTestMovesForFEN(fen, expected, t)
